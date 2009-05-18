a2dc859d98bdf9db53c2ac718e79244f572bd13e
elliottcable <git@elliottcable.name>
2009-05-18 14:08:55 -0800

    Re-colourized everything a tad.

--- theme.lua
+++ theme.lua
@@ -3,15 +3,15 @@ require("winsome")
 theme = winsome.theme("fresh")
 
 -- Colours
-theme.bg_normal = "#25B9F9" -- Sky blue
-theme.fg_normal = "#333333" -- Dark grey
+theme.bg_normal = "#95D9F9" -- Sky blue
+theme.fg_normal = "#222222" -- Dark grey
 theme.border_normal = theme.bg_normal
 
-theme.bg_focus  = "#25F9B9" -- Mint green
-theme.fg_focus  = theme.fg_normal
+theme.bg_focus  = "#052925" -- Forest green
+theme.fg_focus  = "#F5F5F5" -- Bright silver
 theme.border_focus = theme.bg_focus
 
-theme.bg_urgent = "#FF0000" -- Painful red
+theme.bg_urgent = "#550077" -- Painful purple
 theme.fg_urgent = theme.fg_normal
 theme.border_urgent = theme.bg_urgent
 
